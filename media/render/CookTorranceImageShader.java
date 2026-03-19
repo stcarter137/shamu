7c115085a5dc82467903bf7a1cd7ba7c596c7c12
scott carter <scottc@scotts-MacBook-Pro.local>
2026-03-19 09:11:11 -0700

    Reverted last  change to CookTorranceImageShader.java

--- media/render/CookTorranceImageShader.java
+++ media/render/CookTorranceImageShader.java
@@ -188,8 +188,6 @@ microfacetRoughness = 4.8f;
 
    protected void handleBidirectionaIllumination(float barycentricU, float barycentricV, float barycentricW, float[] sample) {
 
-      System.out.println("Testing repository updating")
-
       float[] normal = new float[3];
       float[] position = new float[3];
 
